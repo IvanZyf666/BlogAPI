10ba68312b844a0beb693b16663e5a792699efed
Ivan Zhang <izhang@seisware.com>
2022-06-03 11:54:06 -0600

    added p5

--- Blog/Blog.IService/IBaseService.cs
+++ Blog/Blog.IService/IBaseService.cs
@@ -1,32 +1,32 @@
 ﻿using SqlSugar;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blog.IRepository
+namespace Blog.IService
 {
-    public interface IBaseRepository<TEntity> where TEntity : class, new()
+    public interface IBaseService<TEntity> where TEntity : class, new()
     {
         Task<bool> CreateAsync(TEntity entity);
         Task<bool> DeleteAsync(int id);
         Task<bool> EditAsync(TEntity entity);
         Task<TEntity> FindAsync(int id);
-        Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> func);
         /// <summary>
-        /// 查询全部的数据
+        /// Query all data
         /// </summary>
         /// <returns></returns>
         Task<List<TEntity>> QueryAsync();
         /// <summary>
-        /// 自定义条件查询
+        /// customized query
         /// </summary>
         /// <param name="func"></param>
         /// <returns></returns>
         Task<List<TEntity>> QueryAsync(Expression<Func<TEntity, bool>> func);
         /// <summary>
-        /// 分页查询
+        /// paging query fenye
         /// </summary>
         /// <param name="page"></param>
         /// <param name="size"></param>
@@ -34,7 +34,7 @@ namespace Blog.IRepository
         /// <returns></returns>
         Task<List<TEntity>> QueryAsync(int page, int size, RefAsync<int> total);
         /// <summary>
-        /// 自定义条件分页查询
+        /// customized paging query
         /// </summary>
         /// <param name="func"></param>
         /// <param name="page"></param>
--- Blog/MyBlog.Repository/BaseRepository.cs
+++ Blog/MyBlog.Repository/BaseRepository.cs
@@ -1,35 +1,34 @@
-﻿using Blog.IRepository;
-using Blog.Model;
+﻿
+using MyBlog.IRepository;
+using MyBlog.Model;
 using SqlSugar;
 using SqlSugar.IOC;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Blog.Repository
+namespace MyBlog.Repository
 {
     public class BaseRepository<TEntity> : SimpleClient<TEntity>, IBaseRepository<TEntity> where TEntity : class, new()
     {
         public BaseRepository(ISqlSugarClient context = null) : base(context)
         {
             base.Context = DbScoped.Sugar;
-            // create database
-            base.Context.DbMaintenance.CreateDatabase();
-            // create class
-            base.Context.CodeFirst.SetStringDefaultLength(200).InitTables(
-                typeof(BlogNews),
-                typeof(TypeInfo),
-                typeof(AuthorInfo)
-                );
+            //// 创建数据库
+            //base.Context.DbMaintenance.CreateDatabase();
+            //// 创建表
+            //base.Context.CodeFirst.InitTables(
+            //  typeof(BlogNews),
+            //  typeof(TypeInfo),
+            //  typeof(WriterInfo)
+            //  );
         }
         public async Task<bool> CreateAsync(TEntity entity)
         {
             return await base.InsertAsync(entity);
         }
-
         public async Task<bool> DeleteAsync(int id)
         {
             return await base.DeleteByIdAsync(id);
@@ -39,33 +38,38 @@ namespace Blog.Repository
         {
             return await base.UpdateAsync(entity);
         }
-
-        public async Task<TEntity> FindAsync(int id)
+        //导航查询
+        public virtual async Task<TEntity> FindAsync(int id)
         {
             return await base.GetByIdAsync(id);
         }
 
-        public async Task<List<TEntity>> QueryAsync()
+        public async Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> func)
+        {
+            return await base.GetSingleAsync(func);
+        }
+
+        public virtual async Task<List<TEntity>> QueryAsync()
         {
             return await base.GetListAsync();
         }
 
-        public async Task<List<TEntity>> QueryAsync(Expression<Func<TEntity, bool>> func)
+        public virtual async Task<List<TEntity>> QueryAsync(Expression<Func<TEntity, bool>> func)
         {
             return await base.GetListAsync(func);
         }
 
-        public async Task<List<TEntity>> QueryAsync(int page, int size, RefAsync<int> total)
+        public virtual async Task<List<TEntity>> QueryAsync(int page, int size, RefAsync<int> total)
         {
             return await base.Context.Queryable<TEntity>()
-                .ToPageListAsync(page, size, total);
+              .ToPageListAsync(page, size, total);
         }
 
-        public async Task<List<TEntity>> QueryAsync(Expression<Func<TEntity, bool>> func, int page, int size, RefAsync<int> total)
+        public virtual async Task<List<TEntity>> QueryAsync(Expression<Func<TEntity, bool>> func, int page, int size, RefAsync<int> total)
         {
             return await base.Context.Queryable<TEntity>()
-                .Where(func)
-                .ToPageListAsync(page, size, total);
+              .Where(func)
+              .ToPageListAsync(page, size, total);
         }
     }
 }
